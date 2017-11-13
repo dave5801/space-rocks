7628409d192dd2beffc20f3735552aa758475ec5
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-11-13 10:08:30 -0800

    added templates for jinja pages

--- space_rocks/views/default.py
+++ space_rocks/views/default.py
@@ -2,37 +2,37 @@
 from pyramid.views import view_config
 
 
-@view_config(route_name='home', renderer='.jinja2')
+@view_config(route_name='home', renderer='index.jinja2')
 def home_view(request):
-    """Home view."""
+    """Home view for website."""
     pass
 
 
-@view_config(route_name='about', renderer='.jinja2')
+@view_config(route_name='about', renderer='about_us.jinja2')
 def about_view(request):
-    """Home view."""
+    """Renders about us page."""
     pass
 
 
-@view_config(route_name='size', renderer='.jinja2')
+@view_config(route_name='size', renderer='size_view.jinja2')
 def size_view(request):
-    """Home view."""
+    """Renders view page for the size view."""
     pass
 
 
-@view_config(route_name='distance', renderer='.jinja2')
+@view_config(route_name='distance', renderer='distance_view.jinja2')
 def distance_view(request):
-    """Home view."""
+    """Renders the distance view."""
     pass
 
 
-@view_config(route_name='absmag', renderer='.jinja2')
+@view_config(route_name='absmag', renderer='absmag_view.jinja2')
 def absolute_magnitude_view(request):
-    """Home view."""
+    """Renders the view for absolute mignitude."""
     pass
 
 
-@view_config(route_name='orbits', renderer='.jinja2')
+@view_config(route_name='orbits', renderer='orbits_view.jinja2')
 def orbit_view(request):
-    """Home view."""
+    """Renders the view for the orbits."""
     pass
