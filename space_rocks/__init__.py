b98235aa5c1b5c617d1cee6b5692602b3f82a9f7
Hewlette Dibble <hcodydibble@gmail.com>
2017-11-13 09:56:02 -0800

    minor text fix

--- space_rocks/__init__.py
+++ space_rocks/__init__.py
@@ -5,8 +5,8 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.include('pyramid_jinja2')
+    config.include('.routes')
     config.scan()
     return config.make_wsgi_app()
