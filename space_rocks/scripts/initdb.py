c532a9c4ca66e64230a30b34bd629e0c6d3a8ee2
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-11-14 10:17:59 -0800

    setup frame for database

--- space_rocks/scripts/initdb.py
+++ space_rocks/scripts/initdb.py
@@ -38,3 +38,53 @@ def main(argv=sys.argv):
     Base.metadata.create_all(engine)
 
     session_factory = get_session_factory(engine)
+
+    with transaction.manager:
+        dbsession = get_tm_session(session_factory, transaction.manager)
+        size_models = []
+        distance_models = []
+        absolute_magnitude_models = []
+        orbit_models = []
+        for item in SUPER_DICT:
+            new_size = Size(
+                id=item["id"],
+                name=item["name"],
+                kilometers=item["diakm"],
+                meters=item["diam"],
+                miles=item["diamiles"],
+                feet=item["diafeet"],
+            )
+            size_models.append(new_size)
+            new_distance = Distance(
+                id=item["id"],
+                name=item["name"],
+                astronomical=item["miss_astronomical"],
+                lunar=item["miss_lunar"],
+                kilometers=item["miss_km"],
+                miles=item["miss_miles"],
+            )
+            distance_models.append(new_distance)
+            new_absmag = AbsoluteMag(
+                id=item["id"],
+                name=item["name"],
+                absolutemag=item["absmag"],
+                velocity_kps=item["velocity_kps"],
+                velocity_kph=item["velocity_kph"],
+                velocity_mph=item["velocity_mph"],
+            )
+            absolute_magnitude_models.append(new_absmag)
+            new_orbit = Orbit(
+                id=item["id"],
+                name=item["name"],
+                orbit_period=item["orbit_period"],
+                perihelion_dist=item["perihelion_dist"],
+                aphelion_dist=item["aphelion_dist"],
+                eccentricity=item["orbit_eccentricity"],
+                perihelion_time=item["perihelion_time"],
+            )
+            orbit_models.append(new_orbit)
+        dbsession.add_all(
+            size_models,
+            distance_models,
+            absolute_magnitude_models,
+            orbit_models)
