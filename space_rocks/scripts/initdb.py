7e3ce5430dcb12c638195e70ca93ca5863866c5e
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-11-14 11:51:36 -0800

    changed up database function

--- space_rocks/scripts/initdb.py
+++ space_rocks/scripts/initdb.py
@@ -45,46 +45,47 @@ def main(argv=sys.argv):
         distance_models = []
         absolute_magnitude_models = []
         orbit_models = []
-        for item in SUPER_DICT:
-            new_size = Size(
-                id=int(item["id"]),
-                name=item["name"],
-                kilometers=item["diakm"],
-                meters=item["diam"],
-                miles=item["diamiles"],
-                feet=item["diafeet"],
-            )
-            size_models.append(new_size)
-            new_distance = Distance(
-                id=int(item["id"]),
-                name=item["name"],
-                astronomical=item["miss_astronomical"],
-                lunar=item["miss_lunar"],
-                kilometers=item["miss_km"],
-                miles=item["miss_miles"],
-            )
-            distance_models.append(new_distance)
-            new_absmag = AbsoluteMag(
-                id=int(item["id"]),
-                name=item["name"],
-                absolutemag=item["absmag"],
-                velocity_kps=item["velocity_kps"],
-                velocity_kph=item["velocity_kph"],
-                velocity_mph=item["velocity_mph"],
-            )
-            absolute_magnitude_models.append(new_absmag)
-            new_orbit = Orbit(
-                id=int(item["id"]),
-                name=item["name"],
-                orbit_period=item["orbit_period"],
-                perihelion_dist=item["perihelion_dist"],
-                aphelion_dist=item["aphelion_dist"],
-                eccentricity=item["orbit_eccentricity"],
-                perihelion_time=item["perihelion_time"],
-            )
-            orbit_models.append(new_orbit)
-        dbsession.add_all(
-            size_models,
-            distance_models,
-            absolute_magnitude_models,
-            orbit_models)
+        with open('space_data.pkl', 'rb') as SUPER_DICT:
+            for item in SUPER_DICT:
+                new_size = Size(
+                    neo_id=item["neo_id"],
+                    name=item["name"],
+                    kilometers=item["diakm"],
+                    meters=item["diam"],
+                    miles=item["diamiles"],
+                    feet=item["diafeet"],
+                )
+                size_models.append(new_size)
+                new_distance = Distance(
+                    neo_id=item["neo_id"],
+                    name=item["name"],
+                    astronomical=item["miss_astronomical"],
+                    lunar=item["miss_lunar"],
+                    kilometers=item["miss_km"],
+                    miles=item["miss_miles"],
+                )
+                distance_models.append(new_distance)
+                new_absmag = AbsoluteMag(
+                    neo_id=item["neo_id"],
+                    name=item["name"],
+                    absolutemag=item["absmag"],
+                    velocity_kps=item["velocity_kps"],
+                    velocity_kph=item["velocity_kph"],
+                    velocity_mph=item["velocity_mph"],
+                )
+                absolute_magnitude_models.append(new_absmag)
+                new_orbit = Orbit(
+                    neo_id=item["neo_id"],
+                    name=item["name"],
+                    orbit_period=item["orbit_period"],
+                    perihelion_dist=item["perihelion_dist"],
+                    aphelion_dist=item["aphelion_dist"],
+                    eccentricity=item["orbit_eccentricity"],
+                    perihelion_time=item["perihelion_time"],
+                )
+                orbit_models.append(new_orbit)
+            dbsession.add_all(
+                size_models,
+                distance_models,
+                absolute_magnitude_models,
+                orbit_models)
