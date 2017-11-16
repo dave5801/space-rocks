e79a78c3abadd933bf28aaa4a5d26cae6bef25c8
David Franklin <dave5801@gmail.com>
2017-11-16 11:46:26 -0800

    tests for valid data

--- space_rocks/tests.py
+++ space_rocks/tests.py
@@ -11,7 +11,6 @@ def dummy_request():
     return DummyRequest()
 
 
-
 def test_abs_magnitude_graph_no_arguments_returns_exception():
     """Test if absolute magnitude graph's raises exception, no args."""
     from space_rocks.views.plot_magnitude import graph_abs_magnitude
@@ -54,10 +53,10 @@ def test_abs_magnitude_graph_missing_neo_names_returns_exception():
     test_mag = [1, 2, 3, 4, 5]
     test_vel = [2, 5, 8, 2, 7]
     with pytest.raises(UnknownAxisException):
-        graph_abs_magnitude(test_mag, test_vel,[])
+        graph_abs_magnitude(test_mag, test_vel, [])
 
 
-def test_abs_magnitude_graph_exists_simple_arguments():
+def test_abs_magnitude_graph_exists_valid_arguments():
     """Test if absolute magnitude scatter plot is generated with arguments passed."""
     from space_rocks.views.plot_magnitude import graph_abs_magnitude
 
@@ -67,45 +66,3 @@ def test_abs_magnitude_graph_exists_simple_arguments():
 
     graph_abs_magnitude(test_mag, test_vel, test_neo_names)
     assert os.path.isfile("static/abs_magnitude.html")
-
-'''
-def test_abs_magnitude_graph_exists_no_arguments():
-    """Test if absolute magnitude scatter plot is generated with no arguments passed."""
-    from space_rocks.views.plot_magnitude import graph_abs_magnitude
-    graph_abs_magnitude()
-    assert os.path.isfile("static/abs_magnitude.html") 
-
-
-def test_abs_magnitude_graph_exists_no_arguments():
-    """Test if absolute magnitude scatter plot is generated with empty arguments passed."""
-    from space_rocks.views.plot_magnitude import graph_abs_magnitude
-    graph_abs_magnitude([],[])
-    assert os.path.isfile("static/abs_magnitude.html")
-
-
-
-
-
-def test_abs_magnitude_graph_exists_real_arguments_arguments():
-    """Test if absolute magnitude scatter plot is generated with real-world arguments arguments passed."""
-    from space_rocks.views.plot_magnitude import graph_abs_magnitude
-
-    test_absolute_magnitude_h = [26.5, 26.0, 27.0, 20.3, 25.0, 27.2, 21.0, 23.2, 24.4, 20.8]
-
-    test_relative_velocity = [7.4542360107, 13.7256677343, 13.2380539495, 9.0509437584, 19.7887217851, 7.2800549558, 12.0981897733, 9.355840583, 11.0721262621, 17.4263077305]
-
-    graph_abs_magnitude(test_absolute_magnitude_h, test_relative_velocity)
-    assert os.path.isfile("static/abs_magnitude.html")
-
-
-def test_abs_magnitude_graph_exists_arguments_x_axis_different_len():
-    """Test if absolute magnitude scatter plot is generated with real-world arguments arguments passed."""
-    from space_rocks.views.plot_magnitude import graph_abs_magnitude
-
-    test_absolute_magnitude_h = [26.5, 26.0, 27.0, 20.3, 25.0]
-
-    test_relative_velocity = [7.4542360107, 13.7256677343, 13.2380539495, 9.0509437584, 19.7887217851, 7.2800549558, 12.0981897733, 9.355840583, 11.0721262621, 17.4263077305]
-
-    with pytest.raises(UnknownAxisException):
-        graph_abs_magnitude(test_absolute_magnitude_h, test_relative_velocity)
-        '''
--- space_rocks/views/plot_magnitude.py
+++ space_rocks/views/plot_magnitude.py
@@ -68,61 +68,6 @@ def graph_abs_magnitude(abs_mag=None, velocity=None, neo_names=None):
         raise UnknownAxisException
 
 
-
-    """Create absolute file path for embedded html graph."""
-    '''
-    here = os.path.abspath(__file__)
-    graph_file_path = os.path.join(os.path.dirname(os.path.dirname(here)),"static/abs_magnitude.html")
-
-    output_file(graph_file_path)
-
-    p = figure(
-        title="Brightness and Velocity", tools="tap",
-        x_axis_label='Absolute Magnitude', y_axis_label='Velocity km/s'
-    )
-
-    source = ColumnDataSource(data=dict(
-        x=abs_mag,
-        y=velocity,
-        names=neo_names,
-        fonts=[
-            '<i>italics</i>',
-            '<pre>pre</pre>',
-            '<b>bold</b>',
-            '<small>small</small>',
-            '<del>del</del>'
-        ]
-    ))
-
-    hover = HoverTool(tooltips="""
-        <div>
-            <div>
-            </div>
-            <div>
-                <span style="font-size: 17px; font-weight: lighter;">Name of NEO: </span>
-                <span style="font-size: 17px; font-weight: bold;">@names</span>
-                <span style="font-size: 15px; color: #966;">[$index]</span>
-            </div>
-            <div>
-                <span style="font-size: 17px; font-weight: lighter;">Absolute Magnitude: </span>
-                <span style="font-size: 17px; font-weight: bold;">@x</span>
-            </div>
-            <div>
-                <span style="font-size: 17px; font-weight: lighter;">Relative Velocity: </span>
-                <span style="font-size: 17px; font-weight: bold;">@y</span>
-            </div>
-        </div>
-        """)
-
-    p = figure(plot_width=400, plot_height=400, tools=[hover],
-               title="Brightness and Velocity", 
-               x_axis_label='Absolute Magnitude', y_axis_label='Velocity km/s')
-
-    p.circle('x', 'y', size=20, source=source)
-
-    save(p)
-    '''
-
 if __name__ == '__main__':
     mag = [1, 2, 3, 4, 5]
     vel = [2, 5, 8, 2, 7]
